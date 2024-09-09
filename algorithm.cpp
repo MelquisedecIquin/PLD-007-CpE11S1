9a7df0791ccad23d57c3296166bfb2203d7eda5f
Melquisedec <qmdiquin@tip.edu.ph>
2024-09-09 12:34:33 +0800

    Add files via upload

---
+++ algorithm.cpp
@@ -0,0 +1,21 @@
+#include <iostream> 
+using namespace std; 
+
+int main() 
+{ 
+	int x = 4 , y = 2, sum,difference,product,quotient,remainder; 
+	
+sum= x + y; 
+difference= x - y; 
+product= x * y; 
+quotient= x / y; 
+remainder= x % y; 
+
+cout <<"Sum is = " << sum << endl; 
+cout <<"Difference is = " << difference << endl; 
+cout <<"Product is = " << product << endl; 
+cout <<"Quotient is = " << quotient << endl; 
+cout <<"Remainder is = " << remainder << endl; 
+
+return 0; 
+} 
