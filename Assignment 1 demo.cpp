a29b30a0a8b272c8b2bbe4c448f5fa7c7590cbec
Melquisedec <qmdiquin@tip.edu.ph>
2024-09-09 12:34:05 +0800

    Add files via upload

---
+++ Assignment 1 demo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int num1, num2, num3;
+   
+    cout << "Input Numbers =" << endl;
+    cin >>  num1 >> num2 >> num3;
+   
+    cout << "SUM = " << num1 + num2 + num3
+      << "DIFFERENCE = " << num1 - num2 - num3
+      << "PRODUCT = " << num1 * num2 * num3
+      << "QUOTIENT = " << num1/num2/num3 << endl;
+   
+  return 0;
+}
